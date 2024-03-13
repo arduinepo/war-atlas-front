4ece23a523cccbb3f6cf41ee2355be926d34366f
arduinepo <patrice.andrieu6@gmail.com>
2024-03-13 22:27:59 +0100

    import

--- src/app/peirce-map/peirce-map.component.ts
+++ src/app/peirce-map/peirce-map.component.ts
@@ -56,6 +56,79 @@ export class PeirceMapComponent implements AfterViewInit{
       });
   }
 
+  ngAfterViewInit(): void {
+    this.svg=d3.select("svg");
+    let width = +window.innerWidth,
+    height = +window.innerHeight;
+    this.d3x=window['d3'];
+    let data:any={type:type,objects:objects,arcs:arcs,bbox:bbox,transform:transform};
+    data=feature(data,"land");
+    console.log(data);
+    let proj:any=this.d3x.geoPeirceQuincuncial().rotate([-90,-90,-20]).fitSize([width, height],{type:"Sphere"})
+    let d:any=d3.geoPath().projection(proj);
+    let path=d3.geoPath(proj);
+    let graticule = path(d3.geoGraticule10());
+
+    let svg2:any=document.getElementById("svg");
+
+    this.svg.attr('viewBox',"0 0 "+width+" "+height);
+    this.svgContent=this.svg.append("g").attr("transform-origin",""+width/2+" "+height/2).attr("fill","red");
+
+    let land=this.svgContent.append("path")
+              .attr("d", path(data));
+        
+              let grat=this.svgContent.append("path").attr("d",graticule).attr("fill","none")
+              .attr("stroke","#000").attr("stroke-width","0.1");
+
+    this.svg.on("dblclick",(evt:any)=>{
+      let pt=new DOMPointReadOnly(evt.clientX,evt.clientY);
+      let cursorpt =  pt.matrixTransform(svg2.getScreenCTM()?.inverse());
+      let dims=this.svg.attr('viewBox').split(/\s+|,/);
+      let x=cursorpt.x-Number(dims[2])/4,
+          y=cursorpt.y-Number(dims[3])/4,
+          w=Number(dims[2])/2,h=Number(dims[3])/2;
+      this.svg.attr('viewBox',""+x+" "+y+" "+w+" "+h);
+      this.r/=2;
+      this.strokeWidth/=2;
+      this.currentMarkers.forEach((m)=>m.attr('r',this.r).attr('stroke-width',this.strokeWidth));
+    });
+
+    this.svg.on("wheel",(evt:WheelEvent)=>{
+      evt.preventDefault();
+      if (evt.deltaY<0)
+        this.zoom4();
+      else this.dezoom4();
+    });
+
+    this.svg.on("mousedown",()=>
+      this.moving=true
+    );
+
+    this.svg.on("mousemove",(evt:MouseEvent)=>{
+      if(this.moving){
+        let dims=this.svg.attr('viewBox').split(/\s+|,/);
+        let x=Number(dims[0])-evt.movementX,
+            y=Number(dims[1])-evt.movementY,
+            w=dims[2],h=dims[3];
+        this.svg.attr('viewBox',""+x+" "+y+" "+w+" "+h);
+      }
+    });
+
+    this.svg.on("mouseup",()=>this.moving=false);
+
+    this.supply.getConflictsSubject().subscribe((wars)=>{
+      wars.forEach(w=>{
+        let point=proj([w.ethnie.longitude,w.ethnie.latitude]);
+        this.currentMarkers.push(
+          this.svgContent.append("circle").attr("r",this.r).attr('cx',point[0])
+          .attr('cy',point[1]).attr('fill','purple').attr('stroke','white')
+          .attr("stroke-width",this.strokeWidth)
+          .on("mouseover",(e:any)=>e.target.style.fill="yellow").on("mouseleave",(e:any)=>e.target.style.fill="purple")
+          );
+      });
+    });
+  }
+
   r=10;
   strokeWidth=2;
 
@@ -120,78 +193,7 @@ export class PeirceMapComponent implements AfterViewInit{
 
   moving:boolean=false;
 
-  ngAfterViewInit(): void {
-    this.svg=d3.select("svg");
-    let width = +window.innerWidth,
-    height = +window.innerHeight;
-    this.d3x=window['d3'];
-    let data:any={type:type,objects:objects,arcs:arcs,bbox:bbox,transform:transform};
-    data=feature(data,"land");
-    console.log(data);
-    let proj:any=this.d3x.geoPeirceQuincuncial().fitSize([width, height],{type:"Sphere"})
-    let d:any=d3.geoPath().projection(proj);
-    let path=d3.geoPath(proj);
-    let graticule = path(d3.geoGraticule10());
-
-    let svg2:any=document.getElementById("svg");
-
-    this.svg.attr('viewBox',"0 0 "+width+" "+height);
-    this.svgContent=this.svg.append("g").attr("transform-origin",""+width/2+" "+height/2).attr("fill","red");
-
-    let land=this.svgContent.append("path")
-              .attr("d", path(data));
-        
-              let grat=this.svgContent.append("path").attr("d",graticule).attr("fill","none")
-              .attr("stroke","#000").attr("stroke-width","0.1");
-
-    this.svg.on("dblclick",(evt:any)=>{
-      let pt=new DOMPointReadOnly(evt.clientX,evt.clientY);
-      let cursorpt =  pt.matrixTransform(svg2.getScreenCTM()?.inverse());
-      let dims=this.svg.attr('viewBox').split(/\s+|,/);
-      let x=cursorpt.x-Number(dims[2])/4,
-          y=cursorpt.y-Number(dims[3])/4,
-          w=Number(dims[2])/2,h=Number(dims[3])/2;
-      this.svg.attr('viewBox',""+x+" "+y+" "+w+" "+h);
-      this.r/=2;
-      this.strokeWidth/=2;
-      this.currentMarkers.forEach((m)=>m.attr('r',this.r).attr('stroke-width',this.strokeWidth));
-    });
-
-    this.svg.on("wheel",(evt:WheelEvent)=>{
-      evt.preventDefault();
-      if (evt.deltaY<0)
-        this.zoom4();
-      else this.dezoom4();
-    });
-
-    this.svg.on("mousedown",()=>
-      this.moving=true
-    );
-
-    this.svg.on("mousemove",(evt:MouseEvent)=>{
-      if(this.moving){
-        let dims=this.svg.attr('viewBox').split(/\s+|,/);
-        let x=Number(dims[0])-evt.movementX,
-            y=Number(dims[1])-evt.movementY,
-            w=dims[2],h=dims[3];
-        this.svg.attr('viewBox',""+x+" "+y+" "+w+" "+h);
-      }
-    });
-
-    this.svg.on("mouseup",()=>this.moving=false);
-
-    this.supply.getConflictsSubject().subscribe((wars)=>{
-      wars.forEach(w=>{
-        let point=proj([w.ethnie.longitude,w.ethnie.latitude]);
-        this.currentMarkers.push(
-          this.svgContent.append("circle").attr("r",this.r).attr('cx',point[0])
-          .attr('cy',point[1]).attr('fill','purple').attr('stroke','white')
-          .attr("stroke-width",this.strokeWidth)
-          .on("mouseover",(e:any)=>e.target.style.fill="yellow").on("mouseleave",(e:any)=>e.target.style.fill="purple")
-          );
-      });
-    });
-  }
+  
 
  getAll(){
     this.currentMarkers.forEach(m=>m.remove());
